a1dc09fd16dffdf1ab13cb741a9b775e1bfe69d9
fcpafaa <96966198+fcpafaa@users.noreply.github.com>
2022-01-04 22:32:24 -0300

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,32 @@
+pluginManagement {
+    resolutionStrategy {
+        eachPlugin {
+            if (requested.id.id == "com.android.library") {
+                useModule("com.android.tools.build:gradle:${requested.version}")
+            }
+            if (requested.id.id == "com.android.application") {
+                useModule("com.android.tools.build:gradle:${requested.version}")
+            }
+        }
+    }
+    repositories {
+        gradlePluginPortal()
+        google()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = ("kotlin-android-template")
+
+include(
+    "app",
+    "library-android",
+    "library-kotlin"
+)
